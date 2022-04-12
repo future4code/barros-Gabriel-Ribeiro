4449203108e189e6ae21188350261f20e9781310
Gabriel dos Santos Ribeiro <gabrielsrib@gmail.com>
2022-04-12 19:48:43 -0300

    Atividade_JavaScript

---
+++ modulo1/aula-variaveis/index.js
@@ -0,0 +1,12 @@
+let nick = prompt(" Como quer ser chamado? ")
+
+let number = prompt(" Qual é a seu número da sorte? ")
+
+console.log (typeof nick, nick, typeof number, number)
+
+numeroEscrito = Number(number)
+console.log (numeroEscrito, typeof numeroEscrito)
+
+numeroEscritoString = numeroEscrito.toString()
+console.log (numeroEscritoString, typeof numeroEscritoString)
+
