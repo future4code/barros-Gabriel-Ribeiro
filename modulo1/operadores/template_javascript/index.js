817948629934d2c52ae33278acc9ea35db4d3b12
Gabriel dos Santos Ribeiro <gabrielsrib@gmail.com>
2022-04-14 22:28:50 -0300

    exercício-operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,65 @@
+// EXERCÍCIO OPERADORES
+// 1
+// a = false
+// b = false
+// c = true
+// d = Boolean
+// 2
+// Como as variáveis não foram préviamente declaradas, acabam sendo consideradas string, então vai somar os dois caracteres ao invés de realizar a operação matemática.
+// O resultado seria "2+5=25"
+// 3
+// Uma forma de se resolver seria transformando esses resultados em variáveis do tipo Number, mas criando uma nova variável para tal.
+
+//1
+//let idade1 =prompt("Insira sua idade")
+//let idade2 =prompt("Insira a idade de seu amigo")
+
+//let numero1 = Number(idade1)
+//let numero2 = Number(idade2)
+
+//let maisVelho = numero1 > numero2
+
+//console.log("Sua idade é maior do que a do seu melhor amigo? ---" ,maisVelho)
+
+//let diferença = numero1 - numero2
+
+//console.log("Vocês tem", diferença ,"anos de diferença.")
+
+//2
+//let inputPar =prompt("Insira um número par:")
+//let numeroPar = Number(inputPar)
+//let resto = numeroPar % 2 
+//console.log("O resto da divisão é 0 =",resto)
+
+// Se colocarem um número ímpar o resultado vai ser sempre 1.
+
+//3
+//let idade1 =prompt("Insira sua idade")
+//let idadeNumero = Number(idade1)
+//let meses = idadeNumero * 12
+//let dias = meses * 30
+//let horas = dias * 24
+//console.log("Você tem", meses,"meses de vida, ou",dias,"dias ou se preferir,",horas,"horas.")
+
+//4
+//let entrada1 = prompt("Insira um número:")
+//let entrada2 = prompt("Insira mais um número:")
+
+//let numero1 = Number(entrada1)
+//let numero2 = Number(entrada2)
+
+//let maior = numero1 > numero2
+//console.log("O primeiro numero é maior que segundo?",maior)
+
+//let igual = numero1 == numero2
+//console.log("O primeiro numero é igual ao segundo?",igual)
+
+//let divisao = numero1 % numero2
+//let divisivel = divisao == 0
+//console.log("O primeiro numero é divisível pelo segundo?",divisivel)
+
+//let divisao2 = numero2 % numero1
+//let divisivel2 = divisao2 == 0
+//console.log("O segundo numero é divisível pelo primeiro?",divisivel2)
+
+//
