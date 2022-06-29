91271fa19b5932199eb34754a0f8d1bc065068b5
Gabriel dos Santos Ribeiro <gabrielsrib@gmail.com>
2022-06-29 19:30:30 -0300

    surge

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -134,3 +134,17 @@ qntdChutes++
     console.log(`Você acertou! Isso tomou ${qntdChutes} tentativa(s).`)
 }
 */
+
+function criarArrayNomesAnimais() {
+    const animais = [
+    { nome: "Cachorro", classificacao: "mamífero" },
+    { nome: "Papagaio", classificacao: "ave" },
+    { nome: "Gato", classificacao: "mamífero" },
+    { nome: "Carpa", classificacao: "peixe" },
+    { nome: "Pomba", classificacao: "ave" }
+  ];
+    return([animais.map(getFullName)]);
+  function getFullName(item) {
+    return item.nome;
+  }
+}
