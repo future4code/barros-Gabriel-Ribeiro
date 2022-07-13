edbbb66feb8b072ca1c80810f37c163d0f7511ff
Gabriel dos Santos Ribeiro <gabrielsrib@gmail.com>
2022-07-12 23:12:31 -0300

    tentativa de exercicio

---
+++ modulo3/rend-condicional-form/formulario-condicional/src/components/etapa2.js
@@ -0,0 +1,13 @@
+function Etapa2() {
+    return (
+      <div>
+        <h1>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h1>
+        <h2>5. Qual curso?</h2>
+        <input></input>
+        <h2>6. Qual unidade de ensino?</h2>
+        <input></input>
+      </div>
+    );
+  }
+  
+  export default Etapa2;
---
+++ modulo3/rend-condicional-form/formulario-condicional/src/components/etapa3.js
@@ -0,0 +1,13 @@
+function Etapa3() {
+    return (
+      <div>
+        <h1>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+        <h2>5. Por que você não terminou um curso de graduação?</h2>
+        <input></input>
+        <h2>6. Você fez algum curso complementar?</h2>
+        <input></input>
+      </div>
+    );
+  }
+  
+  export default Etapa3;
