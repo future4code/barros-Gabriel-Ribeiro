6ce1f8ec694ec7969c98d507baae3a5b982ebe59
Gabriel dos Santos Ribeiro <gabrielsrib@gmail.com>
2022-10-28 20:00:32 -0300

    exercicio de aprofundamento do express

---
+++ BackEnd/Modulo1/aprofundamento-express/src/data.ts
@@ -0,0 +1,34 @@
+import * as allTypes from './type'
+
+export const data:allTypes.Afazeres[] = [
+    {
+        idUsuario:'001',
+        id:1,
+        titulo:"Lavar o Banheiro",
+        status:"true"
+    },
+    {
+        idUsuario:'002',
+        id:2,
+        titulo:"Lavar a Louça",
+        status:"false"
+    },
+    {
+        idUsuario:'004',
+        id:3,
+        titulo:"Varrer o chão",
+        status:"true"
+    },
+    {
+        idUsuario:'001',
+        id:4,
+        titulo:"Passar um pano no chão",
+        status:"false"
+    },
+    {
+        idUsuario:'002',
+        id:5,
+        titulo:"Tirar a poeira",
+        status:"false"
+    },
+]
